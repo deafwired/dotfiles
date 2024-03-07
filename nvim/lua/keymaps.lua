ce365a460f1d65ad4bec28e0a496badd3451b95a
matt <mtesamspam@gmail.com>
2024-03-07 12:51:32 -0500

    updating nvim

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,4 +8,3 @@ vim.api.nvim_set_keymap("i", "<A-Up>", "<Esc>:move-2<CR>", { noremap = true, sil
 -- Move line down
 vim.api.nvim_set_keymap("n", "<A-Down>", ":<C-u>move+<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<A-Down>", "<Esc>:move+<CR>", { noremap = true, silent = true })
-
