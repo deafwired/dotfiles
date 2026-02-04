5b78b371a6b9944eed0c31c77da358f7295b643f
Matt <deafwired@proton.me>
2026-02-04 16:29:08 -0500

    add auto-session

---
+++ nvim/lua/plugins/autosession.lua
@@ -0,0 +1,13 @@
+return {
+    "rmagatti/auto-session",
+    lazy = false,
+    opts = {
+        suppressed_dirs = {
+            "~/", "~/Downloads"
+        },
+        session_lens = {
+            load_on_setup = true,
+            previewer = false,
+        },
+    },
+}
